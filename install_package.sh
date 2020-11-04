d0b2cb031e5a9fa788797c9e98e73d59354549b2
Ray <851836818@qq.com>
2020-11-04 14:53:03 +0800

    Update install_package.sh

--- install_package.sh
+++ install_package.sh
@@ -1,14 +1,14 @@
 # Use conda to install latest version of plotly to avoid error
-conda install -c ployly plotly &&\
+conda install -y -c ployly plotly &&\
 # plotly
 pip install plotly==3.10.0  &&\
 # Networkx
-conda install networkx &&\
+conda install -y networkx &&\
 # Biopython
-conda install -c conda-forge biopython &&\
+conda install -y -c conda-forge biopython &&\
 # Numpy
-conda install numpy &&\
+conda install -y numpy &&\
 # Pandas
-conda install pandas
+conda install -y pandas
 
 
