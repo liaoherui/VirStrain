86f4e6386e8fe45f44e0cc095a1ac921a93923b7
Ray <851836818@qq.com>
2020-11-04 14:21:52 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KSVSOo5dc3i7KZZ2ReDaDNLt_Nf2DXw1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KSVSOo5dc3i7KZZ2ReDaDNLt_Nf2DXw1" -O VirStrain_DB.zip && rm -rf /tmp/cookies.txt 
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KSVSOo5dc3i7KZZ2ReDaDNLt_Nf2DXw1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KSVSOo5dc3i7KZZ2ReDaDNLt_Nf2DXw1" -O VirStrain_DB.zip && rm -rf /tmp/cookies.txt  &&\
 
-
-#unzip VirStrain_DB.zip &&\
-#rm  VirStrain_DB.zip
+unzip VirStrain_DB.zip &&\
+rm  VirStrain_DB.zip
