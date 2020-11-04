4a09fc471675e192ed4ceba61b93e6ecad526dfa
Ray <851836818@qq.com>
2020-11-04 12:37:50 +0800

    Add files via upload

---
+++ install_package.sh
@@ -0,0 +1,12 @@
+# plotly
+pip install plotly==3.10.0
+# Networkx
+conda install networkx
+# Biopython
+conda install -c conda-forge biopython
+# Numpy
+conda install numpy
+# Pandas
+conda install pandas
+
+
