263d6880057364faba569f19df5168134c4a63d3
Ray <851836818@qq.com>
2020-11-06 16:37:27 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KSVSOo5dc3i7KZZ2ReDaDNLt_Nf2DXw1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KSVSOo5dc3i7KZZ2ReDaDNLt_Nf2DXw1" -O VirStrain_DB.zip && rm -rf /tmp/cookies.txt  &&\
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Ye2neZEm-RBjEzNwKdkt3VI9OMlmtgLH' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Ye2neZEm-RBjEzNwKdkt3VI9OMlmtgLH" -O VirStrain_DB.tar.gz && rm -rf /tmp/cookies.txt  &&\
 
-gunzip VirStrain_DB.zip &&\
-rm  VirStrain_DB.zip
+tar -zxvf VirStrain_DB.tar.gz &&\
+rm  VirStrain_DB.tar.gz
