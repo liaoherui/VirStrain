f521b8f242cac7667133b2eb11c89785e00c4600
Ray <851836818@qq.com>
2020-11-11 11:23:39 +0800

    Add files via upload

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Ye2neZEm-RBjEzNwKdkt3VI9OMlmtgLH' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Ye2neZEm-RBjEzNwKdkt3VI9OMlmtgLH" -O VirStrain_DB.tar.gz && rm -rf /tmp/cookies.txt  &&\
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18IFeHuf85Qj5378QiFHGUJukxLV_RgYT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18IFeHuf85Qj5378QiFHGUJukxLV_RgYT" -O VirStrain_DB.tar.gz && rm -rf /tmp/cookies.txt  &&\
 
 tar -zxvf VirStrain_DB.tar.gz &&\
 rm  VirStrain_DB.tar.gz
